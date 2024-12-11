88918d1d2315aea29122e3db7f4446185c4aa96f
Veronica <3remes@gmail.com>
2024-12-11 21:39:03 +0800

    change .env

--- server.php
+++ server.php
@@ -10,5 +10,5 @@ if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
 }
 
 require_once __DIR__.'/public/index.php';
-// $port = getenv('PORT') ?: 8000;
-// $host = '0.0.0.0';
+$port = getenv('PORT') ?: 8000;
+$host = '0.0.0.0';

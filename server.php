396a5b420f02fde7bfa366ecd302cff0cec7519f
Veronica <3remes@gmail.com>
2024-12-11 20:56:20 +0800

    change app_env to production

--- server.php
+++ server.php
@@ -10,4 +10,5 @@ if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
 }
 
 require_once __DIR__.'/public/index.php';
-$port = env('PORT', 8080);
+$port = getenv('PORT') ?: 8000;
+$host = '0.0.0.0';

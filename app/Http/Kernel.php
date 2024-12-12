cf7bd43bbe82e3668bbd75073f97f8f778057505
Veronica <3remes@gmail.com>
2024-12-12 13:49:37 +0800

    attempt to fix csrf token mismatch issue

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -28,7 +28,13 @@ class Kernel extends HttpKernel
         'web' => [
             \App\Http\Middleware\VerifyCsrfToken::class,
         ],
+
+        'api' => [
+            'throttle:api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
     ];
+
 
     /**
      * The application's middleware aliases.
